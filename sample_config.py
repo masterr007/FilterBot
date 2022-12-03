ec3540c5d60ce812c42066ff7f531901a543ae3d
jyotirmoydeka <68185650+jyotirmoydeka@users.noreply.github.com>
2022-12-03 14:40:51 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,19 +4,19 @@ import time
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5941372397:AAFvb1yV-yagcjLp9ROQRbBcXr_YxZX4CIQ")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    API_ID = int(os.environ.get("API_ID", "11159699")
+    API_ID = int(os.environ.get("API_ID", 12345))
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    API_HASH = os.environ.get("API_HASH", "b432bad423ae90d470d906ef26109712")
+    API_HASH = os.environ.get("API_HASH", "")
     
     
     # Database URL from https://cloud.mongodb.com/
-    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://nano:nano@cluster0.3045qne.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "")
 
 
     # Your database name from mongoDB
@@ -24,7 +24,7 @@ class Config(object):
 
 
     # ID of users that can use the bot commands
-    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "2120922739").split())
+    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
 
 
     # To save user details (Usefull for getting userinfo and total user counts)
